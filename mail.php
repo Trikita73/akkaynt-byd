367bc152116777f2336bfb562adbc30d5d54067a
andrii <diachenkoandrii7@gmail.com>
2017-01-14 15:47:00 +0200

    akkaynt-byd

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "2andu@mail.ru";
+$sitename = "Акк@унт-Буд";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
